70d371d6f6bff23cfc5abc024f7532d4c04af006
Mac <vespersolutionspk@gmail.com>
2025-04-14 15:00:23 +0500

    carfilio

--- app/homecomponents/Hero.tsx
+++ app/homecomponents/Hero.tsx
@@ -384,7 +384,7 @@ const Hero = () => {
                   <iframe 
                     ref={videoRef}
                     className="absolute top-0 left-0 w-full h-full"
-                    src="https://www.youtube.com/embed/9PWOakf2ZU8?si=oZIaq50xixeIxxtz" 
+                    src="https://www.youtube.com/embed/5NHkd1wiJPI" 
                     title="Carfilio Demo Video"
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
